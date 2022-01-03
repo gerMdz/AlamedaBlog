0fc295986d26f69b3ac7a324b3772e11d6f556fe
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-01-03 07:33:55 -0300

    Change Demo for Alameda Blog

--- assets/app.js
+++ assets/app.js
@@ -11,8 +11,5 @@ import 'jquery'
 // loads the code syntax highlighting library
 import './js/highlight.js';
 
-// Creates links to the Symfony documentation
-import './js/doclinks.js';
-
 // start the Stimulus application
 import './bootstrap';
