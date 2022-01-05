fdbf1be757826f2f9bbd08536699c4f932265e6b
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-01-05 09:40:52 -0300

    Agregando cards

--- assets/app.js
+++ assets/app.js
@@ -2,19 +2,21 @@ import './styles/app.scss';
 
 // loads the Bootstrap jQuery plugins
 // import 'bootstrap-sass/assets/javascripts/bootstrap/transition.js';
-import 'bootstrap/js/dist/alert';
-import 'bootstrap/js/dist/collapse';
-import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/modal';
-// import 'jquery'
-// const $ = require('jquery');
-import $ from 'jquery';
-require('bootstrap');
+
+// import $ from 'jquery';
+import 'bootstrap';
+// import './bootstrap';
+// import { Tooltip, Toast, Popover, Modal, Dropdown } from 'bootstrap';
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
 // loads the code syntax highlighting library
 import './js/highlight.js';
 
 // start the Stimulus application
 import './stimulus-bridge';
+// $('.dropdown-toggle').dropdown();
 
 $(document).ready(function() {
     console.log('wpack-hello')
